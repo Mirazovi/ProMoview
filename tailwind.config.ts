199c19928d73f1a1fc0b483cf95956767b02da82
Mirazovi <mdmirazhossainovi@gmail.com>
2025-04-10 16:29:49 +0600

    ok

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class', // 👈 This enables class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
